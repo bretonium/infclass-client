InfectionDust/teeworlds-reinfected

CInfClassPlayerClass: Add IsHuman/IsInfected getters

src/game/server/infclass/classes/infcplayerclass.cpp
@@ -90,6 +90,11 @@ void CInfClassPlayerClass::SetCharacter(CInfClassCharacter *character)
 	m_pCharacter->SetClass(this);
 }
 
+bool CInfClassPlayerClass::IsInfected() const
+{
+	return !IsHuman();
+}
+
 void CInfClassPlayerClass::SetupSkin()
 {
 	for(int part = 0; part < NUM_SKINPARTS; part++)

bretonium/infclass-client

src/game/server/infclass/classes/infcplayerclass.cpp
@@ -98,6 +98,11 @@ void CInfClassPlayerClass::SetCharacter(CInfClassCharacter *character)
 	m_pCharacter->SetClass(this);
 }
 
+bool CInfClassPlayerClass::IsZombie() const
+{
+	return !IsHuman();
+}
+
 void CInfClassPlayerClass::SetupSkin()
 {
 	for(int part = 0; part < NUM_SKINPARTS; part++)