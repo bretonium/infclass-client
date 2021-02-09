InfectionDust/teeworlds-reinfected

CInfClassPlayerClass: Add initial skin API

src/game/server/infclass/classes/humans/hero.cpp
@@ -6,3 +6,20 @@ CInfClassHero::CInfClassHero()
 	: CInfClassHuman()
 {
 }
+
+void CInfClassHero::SetupSkin()
+{
+	CInfClassHuman::SetupSkin(); // Reset to default
+
+	// Hero (correct colors)
+	str_copy(m_SkinInfo.m_aaSkinPartNames[SKINPART_MARKING], "stripe", sizeof(m_SkinInfo.m_aaSkinPartNames[SKINPART_MARKING]));
+	m_SkinInfo.m_aUseCustomColors[SKINPART_BODY] = 1;
+	m_SkinInfo.m_aUseCustomColors[SKINPART_HANDS] = 1;
+	m_SkinInfo.m_aUseCustomColors[SKINPART_FEET] = 1;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_BODY] = 16307835;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_MARKING] = -16711808;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_DECORATION] = 65408;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_HANDS] = 184;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_FEET] = 9765959;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_EYES] = 65408;
+}

src/game/server/infclass/classes/infcplayerclass.cpp
@@ -1,5 +1,25 @@
 #include "infcplayerclass.h"
 
+#include <base/system.h>
+
 CInfClassPlayerClass::CInfClassPlayerClass()
 {
+}
+
+void CInfClassPlayerClass::SetupSkin()
+{
+	for(int part = 0; part < NUM_SKINPARTS; part++)
+	{
+		if ((part == SKINPART_MARKING) || (part == SKINPART_DECORATION))
+		{
+			str_copy(m_SkinInfo.m_aaSkinPartNames[part], "", sizeof(m_SkinInfo.m_aaSkinPartNames[part]));
+		}
+		else
+		{
+			str_copy(m_SkinInfo.m_aaSkinPartNames[part], "standard", sizeof(m_SkinInfo.m_aaSkinPartNames[part]));
+		}
+
+		m_SkinInfo.m_aUseCustomColors[part] = 0;
+		m_SkinInfo.m_aSkinPartColors[part] = 0;
+	}
 }

src/game/server/infclass/classes/infected/hunter.cpp
@@ -4,3 +4,21 @@ CInfClassHunter::CInfClassHunter()
 	: CInfClassInfected()
 {
 }
+
+void CInfClassHunter::SetupSkin()
+{
+	CInfClassInfected::SetupSkin(); // Reset to default
+
+	// Hunter (rough colors)
+	str_copy(m_SkinInfo.m_aaSkinPartNames[SKINPART_MARKING], "warpaint", sizeof(m_SkinInfo.m_aaSkinPartNames[SKINPART_MARKING]));
+	m_SkinInfo.m_aUseCustomColors[SKINPART_BODY] = 1;
+	m_SkinInfo.m_aUseCustomColors[SKINPART_MARKING] = 1;
+	m_SkinInfo.m_aUseCustomColors[SKINPART_HANDS] = 1;
+	m_SkinInfo.m_aUseCustomColors[SKINPART_FEET] = 1;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_BODY] = 3931970;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_MARKING] = -11993216;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_DECORATION] = 65408;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_HANDS] = 750337;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_FEET] = 750337;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_EYES] = 65408;
+}

src/game/server/infclass/infcplayer.cpp
@@ -32,6 +32,20 @@ void CInfClassPlayer::TryRespawn()
 	GameServer()->CreatePlayerSpawn(SpawnPos);
 }
 
+void CInfClassPlayer::SyncSkin()
+{
+	m_pInfcPlayerClass->SetupSkin();
+	const SkinInfo &skin = m_pInfcPlayerClass->GetSkinInfo();
+
+	for(int p = 0; p < NUM_SKINPARTS; p++)
+	{
+		str_copy(m_TeeInfos.m_aaSkinPartNames[p], skin.m_aaSkinPartNames[p], 24);
+		m_TeeInfos.m_aUseCustomColors[p] = skin.m_aUseCustomColors[p];
+		m_TeeInfos.m_aSkinPartColors[p] = skin.m_aSkinPartColors[p];
+	}
+	m_pInfcGameContext->AnnounceSkinChange(GetCID());
+}
+
 void CInfClassPlayer::SetCharacterClass(int ClassId)
 {
 	CInfClassPlayerClass *pClass = m_pInfcGameContext->CreateInfClass(ClassId);
@@ -44,4 +58,5 @@ void CInfClassPlayer::SetCharacterClass(CInfClassPlayerClass *pClass)
 		delete m_pInfcPlayerClass;
 
 	m_pInfcPlayerClass = pClass;
+	SyncSkin();
 }

bretonium/infclass-client

src/game/server/infclass/classes/humans/hero.cpp
@@ -6,3 +6,20 @@ CInfClassHero::CInfClassHero()
 	: CInfClassHuman()
 {
 }
+
+void CInfClassHero::SetupSkin()
+{
+	CInfClassHuman::SetupSkin(); // Reset to default
+
+	// Hero (correct colors)
+	str_copy(m_SkinInfo.m_apSkinPartNames[SKINPART_MARKING], "stripe", sizeof(m_SkinInfo.m_apSkinPartNames[SKINPART_MARKING]));
+	m_SkinInfo.m_aUseCustomColors[SKINPART_BODY] = 1;
+	m_SkinInfo.m_aUseCustomColors[SKINPART_HANDS] = 1;
+	m_SkinInfo.m_aUseCustomColors[SKINPART_FEET] = 1;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_BODY] = 16307835;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_MARKING] = -16711808;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_DECORATION] = 65408;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_HANDS] = 184;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_FEET] = 9765959;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_EYES] = 65408;
+}

src/game/server/infclass/classes/infcplayerclass.cpp
@@ -1,5 +1,25 @@
 #include "infcplayerclass.h"
 
+#include <base/system.h>
+
 CInfClassPlayerClass::CInfClassPlayerClass()
 {
+}
+
+void CInfClassPlayerClass::SetupSkin()
+{
+	for(int part = 0; part < NUM_SKINPARTS; part++)
+	{
+		if ((part == SKINPART_MARKING) || (part == SKINPART_DECORATION))
+		{
+			str_copy(m_SkinInfo.m_apSkinPartNames[part], "", sizeof(m_SkinInfo.m_apSkinPartNames[part]));
+		}
+		else
+		{
+			str_copy(m_SkinInfo.m_apSkinPartNames[part], "standard", sizeof(m_SkinInfo.m_apSkinPartNames[part]));
+		}
+
+		m_SkinInfo.m_aUseCustomColors[part] = 0;
+		m_SkinInfo.m_aSkinPartColors[part] = 0;
+	}
 }

src/game/server/infclass/classes/infected/hunter.cpp
@@ -4,3 +4,21 @@ CInfClassHunter::CInfClassHunter()
 	: CInfClassInfected()
 {
 }
+
+void CInfClassHunter::SetupSkin()
+{
+	CInfClassInfected::SetupSkin(); // Reset to default
+
+	// Hunter (rough colors)
+	str_copy(m_SkinInfo.m_apSkinPartNames[SKINPART_MARKING], "warpaint", sizeof(m_SkinInfo.m_apSkinPartNames[SKINPART_MARKING]));
+	m_SkinInfo.m_aUseCustomColors[SKINPART_BODY] = 1;
+	m_SkinInfo.m_aUseCustomColors[SKINPART_MARKING] = 1;
+	m_SkinInfo.m_aUseCustomColors[SKINPART_HANDS] = 1;
+	m_SkinInfo.m_aUseCustomColors[SKINPART_FEET] = 1;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_BODY] = 3931970;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_MARKING] = -11993216;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_DECORATION] = 65408;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_HANDS] = 750337;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_FEET] = 750337;
+	m_SkinInfo.m_aSkinPartColors[SKINPART_EYES] = 65408;
+}

src/game/server/infclass/infcplayer.cpp
@@ -32,6 +32,21 @@ void CInfClassPlayer::TryRespawn()
 	GameServer()->CreatePlayerSpawn(SpawnPos);
 }
 
+void CInfClassPlayer::SyncSkin()
+{
+	m_pInfcPlayerClass->SetupSkin();
+	const CTeeInfo &skin = m_pInfcPlayerClass->GetSkinInfo();
+
+	for(int p = 0; p < NUM_SKINPARTS; p++)
+	{
+		str_copy(m_TeeInfos.m_apSkinPartNames[p], skin.m_apSkinPartNames[p], 24);
+		m_TeeInfos.m_aUseCustomColors[p] = skin.m_aUseCustomColors[p];
+		m_TeeInfos.m_aSkinPartColors[p] = skin.m_aSkinPartColors[p];
+	}
+	m_TeeInfos.FromSixup();
+	m_pInfcGameContext->AnnounceSkinChange(GetCID());
+}
+
 void CInfClassPlayer::SetCharacterClass(int ClassId)
 {
 	CInfClassPlayerClass *pClass = m_pInfcGameContext->CreateInfClass(ClassId);
@@ -44,4 +59,5 @@ void CInfClassPlayer::SetCharacterClass(CInfClassPlayerClass *pClass)
 		delete m_pInfcPlayerClass;
 
 	m_pInfcPlayerClass = pClass;
+	SyncSkin();
 }