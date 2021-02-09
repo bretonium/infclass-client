InfectionDust/teeworlds-reinfected

Introduce InfClassR7 game mode

src/game/server/infclass/infcgamecontroller.cpp
@@ -1,0 +1,12 @@
+#include <engine/shared/config.h>
+
+#include <game/server/entities/character.h>
+#include <game/server/gamecontext.h>
+#include <game/server/player.h>
+
+#include "infcgamecontroller.h"
+
+CGameControllerInfClass::CGameControllerInfClass(CGameContext *pGameServer) : IGameController(pGameServer)
+{
+	m_pGameType = "InfClassR7";
+}

src/game/server/infclass/infcgamecontroller.h
@@ -1,0 +1,16 @@
+#ifndef GAME_SERVER_INFCLASS_CONTROLLER_H
+#define GAME_SERVER_INFCLASS_CONTROLLER_H
+
+#include <game/server/gamecontroller.h>
+
+class CGameContext;
+
+class CGameControllerInfClass : public IGameController
+{
+public:
+	CGameControllerInfClass(CGameContext *pGameServer);
+};
+
+extern IGameController *CreateInfCGameServer();
+
+#endif // GAME_SERVER_INFCLASS_CONTROLLER_H

bretonium/infclass-client

src/game/server/infclass/infcgamecontroller.cpp
@@ -1,0 +1,12 @@
+#include <engine/shared/config.h>
+
+#include <game/server/entities/character.h>
+#include <game/server/gamecontext.h>
+#include <game/server/player.h>
+
+#include "infcgamecontroller.h"
+
+CGameControllerInfClass::CGameControllerInfClass(CGameContext *pGameServer) : CGameControllerDDRace(pGameServer)
+{
+	m_pGameType = "InfClassR7";
+}

src/game/server/infclass/infcgamecontroller.h
@@ -1,0 +1,17 @@
+#ifndef GAME_SERVER_INFCLASS_CONTROLLER_H
+#define GAME_SERVER_INFCLASS_CONTROLLER_H
+
+#include <game/server/gamecontroller.h>
+#include <game/server/gamemodes/DDRace.h>
+
+class CGameContext;
+
+class CGameControllerInfClass : public CGameControllerDDRace
+{
+public:
+	CGameControllerInfClass(CGameContext *pGameServer);
+};
+
+extern IGameController *CreateInfCGameServer();
+
+#endif // GAME_SERVER_INFCLASS_CONTROLLER_H